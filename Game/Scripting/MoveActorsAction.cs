212cb00745566d5e80860900a619c8fb52c2a85f
zmacconnell <zachary.macconnell@gmail.com>
2022-11-08 11:03:07 -0700

    Added files from another class to MoveNextActor

--- Game/Scripting/MoveActorsAction.cs
+++ Game/Scripting/MoveActorsAction.cs
@@ -5,7 +5,7 @@ using Unit05.Game.Casting;
 namespace Unit05.Game.Scripting
 {
     // TODO: Implement the MoveActorsAction class here
-
+    
     // 1) Add the class declaration. Use the following class comment. Make sure you
     //    inherit from the Action class.
 
@@ -15,17 +15,52 @@ namespace Unit05.Game.Scripting
     /// The responsibility of MoveActorsAction is to move all the actors.
     /// </para>
     /// </summary>
+    public class MoveActorsAction : Action
+    {
 
+    }
     // 2) Create the class constructor. Use the following method comment.
 
     /// <summary>
     /// Constructs a new instance of MoveActorsAction.
     /// </summary>
+    public MoveActorsAction()
+    {
 
+    }
     // 3) Override the Execute(Cast cast, Script script) method. Use the following 
     //    method comment. You custom implementation should do the following:
     //    a) get all the actors from the cast
     //    b) loop through all the actors
     //    c) call the MoveNext() method on each actor.
+    public void Execute(Cast cast, Script script)
+    {
+        // left
+            if (_keyboardService.IsKeyDown("a"))
+            {
+                _direction = new Point(-Constants.CELL_SIZE, 0);
+            }
 
+            // right
+            if (_keyboardService.IsKeyDown("d"))
+            {
+                _direction = new Point(Constants.CELL_SIZE, 0);
+            }
+
+            // up
+            if (_keyboardService.IsKeyDown("w"))
+            {
+                _direction = new Point(0, -Constants.CELL_SIZE);
+            }
+
+            // down
+            if (_keyboardService.IsKeyDown("s"))
+            {
+                _direction = new Point(0, Constants.CELL_SIZE);
+            }
+
+            Snake snake = (Snake)cast.GetFirstActor("snake");
+            snake.TurnHead(_direction);
+
+    }
 }
